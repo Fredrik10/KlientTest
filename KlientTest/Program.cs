7db6ff2877e41579713be517aef17709626cf86e
elev <elev@5CG62366CS>
2019-01-08 13:22:41 +0100

    @ntitobias

--- KlientTest/Program.cs
+++ KlientTest/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace KlientTest
 {
@@ -10,6 +10,31 @@ namespace KlientTest
     {
         static void Main(string[] args)
         {
+            //Fredrik Habib 
+
+            string address = "127.0.0.1";
+            int port = 8001;
+
+            //Ansluter till servern
+            Console.WriteLine("Ansluter...");
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(address, port);
+            Console.WriteLine("Ansluten!");
+
+            //skriver in meddelandet som ska skickas
+            Console.Write("Skriv in meddelandet: ");
+            String message = Console.ReadLine();
+
+            //konvertera meddelande till ASCII-bytes eller  UTF-8
+            Byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(message);
+
+            //Skicka iväg meddelandet 
+            Console.WriteLine("Skickar...");
+            NetworkStream tcpStream = tcpClient.GetStream();
+            tcpStream.Write(bMessage, 0, bMessage.Length);
+
+            tcpClient.Close();
+
         }
     }
 }
